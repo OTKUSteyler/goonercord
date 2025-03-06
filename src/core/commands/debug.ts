72d12770e28bf852884dd088a11884db945816c8
OTKUSteyler <90369564+OTKUSteyler@users.noreply.github.com>
2025-03-06 14:32:26 +0100

    Update debug.ts

--- src/core/commands/debug.ts
+++ src/core/commands/debug.ts
@@ -17,7 +17,7 @@ export default () => <ApplicationCommand>{
         const info = getDebugInfo();
         const content = [
             "💦**GoonerCord Debug Info**💦",
-            `> Revenge: ${info.bunny.version} (${"CumCord💦 " }${info.bunny.loader.version})`,
+            `> Revenge: ${info.bunny.version} (${"GoonerCord💦 " }${info.bunny.loader.version})`,
             `> Discord: ${info.discord.version} (${info.discord.build})`,
             `> React: ${info.react.version} (RN ${info.react.nativeVersion})`,
             `> Hermes: ${info.hermes.version} (bcv${info.hermes.bytecodeVersion})`,
