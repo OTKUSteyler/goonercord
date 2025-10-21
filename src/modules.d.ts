5c4cafde08bd4546bda754b8db73e830777be928
kmmiio99o <164557248+kmmiio99o@users.noreply.github.com>
2025-10-21 20:44:01 +0200

    idk how to type :3

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,8 +1,8 @@
 declare module "bunny-build-info" {
-    const version: 2.0;
+  const version: "2.0.0";
 }
 
 declare module "*.png" {
-    const str: string;
-    export default str;
+  const str: string;
+  export default str;
 }
